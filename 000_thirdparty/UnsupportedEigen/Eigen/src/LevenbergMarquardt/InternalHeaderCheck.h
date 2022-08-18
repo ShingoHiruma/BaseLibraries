79570ca1d29ce9e2a4dcbdab6fc249a9873dc5a0
satoeu <t_sato-ac@frontier.hokudai.ac.jp>
2022-08-18 15:04:07 +0900

    add

---
+++ 000_thirdparty/UnsupportedEigen/Eigen/src/LevenbergMarquardt/InternalHeaderCheck.h
@@ -0,0 +1,3 @@
+﻿#ifndef EIGEN_LEVENBERGMARQUARDT_MODULE_H
+#error "Please include unsupported/Eigen/LevenbergMarquardt instead of including headers inside the src directory directly."
+#endif
---
+++ 000_thirdparty/UnsupportedEigen/Eigen/src/MoreVectorization/InternalHeaderCheck.h
@@ -0,0 +1,3 @@
+﻿#ifndef EIGEN_MOREVECTORIZATION_MODULE_H
+#error "Please include unsupported/Eigen/MoreVectorization instead of including headers inside the src directory directly."
+#endif
